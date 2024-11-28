c5d8ea05b840b056353ac0b00bae5015d56ae391
VladShi <g3rc@yandex.ru>
2024-11-28 14:55:55 +0300

    Add http status codes for exceptions handlers

--- src/main/java/ru/vladshi/springlearning/exceptions/GlobalExceptionHandler.java
+++ src/main/java/ru/vladshi/springlearning/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package ru.vladshi.springlearning.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import ru.vladshi.springlearning.entities.User;
 
@@ -13,34 +15,42 @@ import static ru.vladshi.springlearning.constants.ViewConstants.*;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserIsAlreadyAuthenticatedException.class)
-    public String handleAccessToPageDeniedException(UserIsAlreadyAuthenticatedException ex) {
+    public String handleAccessToPageDeniedException() {
         return REDIRECT_INDEX_PAGE;
     }
 
     @ExceptionHandler(AuthenticationFailedException.class)
-    public String handleAuthenticationFailedException(AuthenticationFailedException ex) {
+    public String handleAuthenticationFailedException() {
         return REDIRECT_LOGIN;
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
     public ModelAndView handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
         return createModelAndViewForUser(REGISTER_VIEW, ex.getMessage());
     }
 
     @ExceptionHandler(UserValidationOnRegisterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ModelAndView handleUserValidationOnRegister(UserValidationOnRegisterException ex) {
         return createModelAndViewForUser(REGISTER_VIEW, ex.getMessage());
     }
 
     @ExceptionHandler(UserValidationOnLogInException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ModelAndView handleUserValidationOnLogIn(UserValidationOnLogInException ex) {
         return createModelAndViewForUser(LOGIN_VIEW, ex.getMessage());
     }
 
     @ExceptionHandler(InvalidCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ModelAndView handleInvalidCredentials(InvalidCredentialsException ex) {
         return createModelAndViewForUser(LOGIN_VIEW, ex.getMessage());
     }
+// TODO добавить перехватчик неожиданных исключений
+//  @ExceptionHandler(Exception.class)
+//  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//  public String handleGeneralException(Exception ex) { ERROR_VIEW }
 
     private ModelAndView createModelAndViewForUser(String viewName, String errorMessage) {
         ModelAndView modelAndView = new ModelAndView(viewName);

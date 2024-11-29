2b154aad3299722ac07e9fb481345bd315fe1136
VladShi <g3rc@yandex.ru>
2024-11-29 12:39:31 +0300

    Refactor OneToOne to ManyToOne for UserSession
    One user can have multiple sessions. For example, if he uses different
    devices, or different browsers. But one session can have only one owner.

--- src/main/java/ru/vladshi/springlearning/entities/UserSession.java
+++ src/main/java/ru/vladshi/springlearning/entities/UserSession.java
@@ -20,7 +20,7 @@ public class UserSession {
     @Column(name = "expires_at", nullable = false)
     private LocalDateTime expiresAt;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
     private User user;
 }
